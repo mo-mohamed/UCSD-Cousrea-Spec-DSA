2662504654281a6b2536f046d29fe587aac26653
mo-mohamed <mostafa.moniem90@gmail.com>
2021-03-09 02:02:12 -0800

    refills

--- 1-algorithms-toolbox/week-3/number_of_refills.c
+++ 1-algorithms-toolbox/week-3/number_of_refills.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 int refills(int stations[], int n, int l);
+
 int main()
 {
     int distance;
@@ -10,35 +11,39 @@ int main()
     scanf("%d", &distance);
     scanf("%d", &car_travel_cap);
     scanf("%d", &number_of_stations);
-    
+    number_of_stations = number_of_stations + 2;
+
     int stations[number_of_stations];
-    for (int i = 0; i < number_of_stations; i++)
+    stations[0] = 0;
+    stations[number_of_stations - 1] = distance;
+   
+    for (int i = 1; i <= (number_of_stations - 2); i++)
         scanf("%d", &stations[i]);
-    
-    printf("%d \n", refills(stations, number_of_stations, car_travel_cap)); 
+
+    printf("%d \n", refills(stations, number_of_stations - 2, car_travel_cap)); 
+
+    return 0;
 }
 
 
 int refills(int stations[], int n, int l)
 {
-    int current_refill_station = 0;
+    int current_station = 0;
     int num_of_refills = 0;
-    while(current_refill_station <= n)
+    while(current_station <= n)
     {
-        int last_refil_station = current_refill_station;
+        int last_refil_station = current_station;
 
         //Look ahead and chekc distance between current station and last refil station
-        while(current_refill_station <= n && (stations[current_refill_station + 1] - stations[last_refil_station]) <= l)
-            current_refill_station++;
+        while(current_station <= n && (stations[current_station + 1] - stations[last_refil_station]) <= l)
+            current_station++;
 
-        if (current_refill_station <= n)
+        if (current_station <= n)
             num_of_refills++;
-
-         if (current_refill_station >= n)
-            break;
         
-        if(current_refill_station == last_refil_station)
+        if(current_station == last_refil_station)
             return -1;
+
             
     }
     return num_of_refills;
